73850bd6976a9533f768bdba40180f4f5a3f8256
unknown <you@example.com>
2024-07-30 15:24:15 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+
+URL_SERVICE = 'https://cnt-b97189e3-2c08-4720-bda7-ed6c41019b26.containerhub.tripleten-services.com'
+#copia la URL generada
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
